f068f71c7ca4abf299a75cc4eacdda7b24689875
deathbugz <96118116+deathbugz@users.noreply.github.com>
2022-02-08 21:08:28 +0700

    Create redir.php

---
+++ redir/redir.php
@@ -0,0 +1,22 @@
+<?php   
+error_reporting(E_ALL & ~E_NOTICE);  
+$exp = 'Category:';  
+if(!$a = $_GET['a']) $a = 'Vehicles_by_brand';  
+$url = 'https://en.wikipedia.org/w/index.php?title=Special:RandomInCategory/';   
+//echo '<pre>';  
+$j = $i = 1;  
+$urls =  $url.$a;  
+while($i == 1 && $j < 5){  
+    $head = get_headers($urls,1);  
+    $headl = $head['Location'];  
+    //echo $head['Location']. " | i:$i j:$j | ";  
+    if(!strpos($headl,$exp)) $i = 2;  
+    $j++;  
+    $urls = $url.end(explode($exp,$headl));  
+    //echo " $urls<hr / >";   
+}  
+if($j < 5){  
+    header("location: $headl");  
+}  
+end();  
+?> 
